2a72ecba4564052c55ad2a5cc14ec838023c06fa
lawu103 <l58wu@uwaterloo.ca>
2020-10-12 12:15:12 -0400

    Comments and variable renaming

--- src/objects.cpp
+++ src/objects.cpp
@@ -7,11 +7,15 @@
 
 #include "objects.h"
 
+/*
+ * Solves a quadratic for the intersections of the ray with the sphere, as shown in
+ * https://gabrielgambetta.com/computer-graphics-from-scratch/basic-ray-tracing.html
+ */
 bool Sphere::ray_intersects(const Vec3f& origin, const Vec3f& dir) const {
-	const Vec3f centerToOrigin = origin - center;
+	const Vec3f CO = origin - center;
 	float a = dir*dir;
-	float b = 2*(centerToOrigin*dir);
-	float c = centerToOrigin*centerToOrigin - radius*radius;
+	float b = 2*(CO*dir);
+	float c = CO*CO - radius*radius;
 	float discriminant = b*b - 4*a*c;
 	if (discriminant < 0) {
 		return false;
--- src/tiny_raytracer.cpp
+++ src/tiny_raytracer.cpp
@@ -15,11 +15,16 @@
 
 using namespace std;
 
-Vec3f canvasToPort(float canvasX, float canvasY, int canvasWidth, int canvasHeight,
-		int portWidth, int portHeight, int portDepth) {
-	return Vec3f(canvasX*portWidth/canvasWidth, canvasY*portHeight/canvasHeight, portDepth);
+/*
+ * Converts a canvas xy position to a viewport xyz position
+ */
+Vec3f canvasToPort(float canvasX, float canvasY, int canvasW, int canvasH, int portW, int portH, int portD) {
+	return Vec3f(canvasX*portW/canvasW, canvasY*portH/canvasH, portD);
 }
 
+/*
+ * Returns the color a ray sees
+ */
 Vec3f traceRay(const Vec3f& camera, const Vec3f& dir, const Sphere& ball) {
 	if (ball.ray_intersects(camera, dir)) {
 		return ball.get_color();
@@ -30,32 +35,37 @@ Vec3f traceRay(const Vec3f& camera, const Vec3f& dir, const Sphere& ball) {
 int main() {
 	Sphere ball(Vec3f(0, -1, 3), 1, Vec3f(255, 0, 0));
 
+	// Camera position in world coordinates
 	const Vec3f camera(0, 0, 0);
-	const int canvasWidth = 1024;
-	const int canvasHeight = 1024;
-	const int portWidth = 1;
-	const int portHeight = 1;
-	const int portDepth = 1;
 
-	vector<Vec3f> framebuffer(canvasWidth*canvasHeight);
+	// Canvas dimensions
+	const int canvasW = 1024;
+	const int canvasH = 1024;
+
+	// Viewport dimensions in world units. Note that the viewport is always fixed centred to the camera, so
+	// portD is a depth that's relative to the camera.
+	const int portW = 1;
+	const int portH = 1;
+	const int portD = 1;
+
+	vector<Vec3f> framebuffer(canvasW*canvasH);
 
 	// Fill in color values
-	for (int j = 0; j < canvasHeight; ++j) {
-		for (int i = 0; i < canvasWidth; ++i) {
-			float canvasX = i - canvasWidth/2.f;
-			float canvasY = canvasHeight/2.f - j;
-			Vec3f dir = canvasToPort(canvasX, canvasY, canvasWidth, canvasHeight,
-					portWidth, portHeight, portDepth);
+	for (int j = 0; j < canvasH; ++j) {
+		for (int i = 0; i < canvasW; ++i) {
+			float canvasX = i - canvasW/2.f;
+			float canvasY = canvasH/2.f - j;
+			Vec3f dir = canvasToPort(canvasX, canvasY, canvasW, canvasH, portW, portH, portD);
 			Vec3f color = traceRay(camera, dir, ball);
-			framebuffer[j*canvasWidth + i] = color;
+			framebuffer[j*canvasW + i] = color;
 		}
 	}
 
 	// Output to file
 	ofstream ofs;
 	ofs.open("./test.ppm", ofstream::binary);
-	ofs << "P6\n" << canvasWidth << " " << canvasHeight << "\n255\n";
-	for (int i = 0; i < canvasWidth*canvasHeight; ++i) {
+	ofs << "P6\n" << canvasW << " " << canvasH << "\n255\n";
+	for (int i = 0; i < canvasW*canvasH; ++i) {
 		for (int j = 0; j < 3; ++j) {
 			ofs << (char)(framebuffer[i][j]);
 		}
